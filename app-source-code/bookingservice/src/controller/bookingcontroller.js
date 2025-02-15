54ae10884500f12a8b9fbff787ebadffa5c3d05c
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-15 17:43:27 +0100

    booking-controller

---
+++ app-source-code/bookingservice/src/controller/bookingcontroller.js
@@ -0,0 +1,24 @@
+const { createBooking } = require("../models/bookingModel");
+const { publishNewRideRequest } = require("../services/kafkaProducer");
+const { invokeDriverMatching } = require("../services/daprClient");
+
+
+exports.bookRide = async (req, res) => {
+  try {
+    const { userId, pickup, dropoff } = req.body;
+    
+  
+    const newBooking = await createBooking(userId, pickup, dropoff);
+
+    // Publish event to Kafka
+    await publishNewRideRequest(newBooking);
+
+    // Call Driver Matching Service via Dapr
+    await invokeDriverMatching(newBooking);
+
+    res.status(201).json({ message: "Ride booked successfully", newBooking });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
